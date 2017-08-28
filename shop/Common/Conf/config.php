9a22384bec7e7e937e427c19ad7a026f94194527
elephant2 <2224168716@qq.com>
2017-08-28 18:30:31 +0800

    commit point

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'SHOW_PAGE_TRACE'=>true,
+
+    //设置请求的默认分组
+    'DEFAULT_MODULE' => 'Home',
+    'MODULE_ALLOW_LIST' => array('Home','Admin'),//设置一个对比的分组列表???
 );
