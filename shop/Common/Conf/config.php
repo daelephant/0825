ac3c51fdfff8ecaffff0a1ad3c24b4d92f5cf79d
family <2224168716@qq.com>
2017-08-29 07:52:44 +0800

    commit

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -4,6 +4,6 @@ return array(
     'SHOW_PAGE_TRACE'=>true,
 
     //设置请求的默认分组
-    'DEFAULT_MODULE' => 'Home',
-    'MODULE_ALLOW_LIST' => array('Home','Admin'),//设置一个对比的分组列表???
+    'DEFAULT_MODULE' => 'Home',//和下面的列表对比。相当于分组只有Home和Admin，其他的都默认为控制器
+    'MODULE_ALLOW_LIST' => array('Home','Admin'),//设置一个对比的分组列表
 );
