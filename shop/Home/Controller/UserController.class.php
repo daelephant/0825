74226ff1e6a8153f70b71616a9c529606976dda8
elephant2 <2224168716@qq.com>
2017-08-28 16:31:44 +0800

    Initial commit

---
+++ shop/Home/Controller/UserController.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyx
+ * Date: 2017-08-28
+ * Time: 11:17
+ */
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller{
+    //登录系统
+    function login(){
+//        echo "登录";
+          $this->display();
+    }
+    //注册系统
+    function register(){
+        echo "注册";
+    }
+}
---
+++ shop/index.php
@@ -0,0 +1,13 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+define('APP_DEBUG',true);//设置debug模式一定要放在加载项目之前
+
+
+//给系统静态资源文件请求路径设置常量
+define('CSS_URL','/shop/Home/Public/css/');
+define('IMG_URL','/shop/Home/Public/images/');
+define('JS_URL','/shop/Home/Public/js/');
+
+
+//引入tp框架接口文件
+include ("../ThinkPHP/ThinkPHP.php");
