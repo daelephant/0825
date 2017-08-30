db9618db0add7fbb113d7077e77943d119e6e227
family <2224168716@qq.com>
2017-08-31 07:16:35 +0800

    commit

---
+++ shop/Runtime/Cache/3f5ffac7ca9378b74c1b94216d70bdb5d23fcf05.file.index.html.php
@@ -0,0 +1,39 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2017-08-29 07:51:00
+         compiled from "D:/phpStudy/WWW/0825/shop/Home/View\Index\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:1827459a4ac64016d05-21904573%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3f5ffac7ca9378b74c1b94216d70bdb5d23fcf05' => 
+    array (
+      0 => 'D:/phpStudy/WWW/0825/shop/Home/View\\Index\\index.html',
+      1 => 1503964256,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1827459a4ac64016d05-21904573',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_59a4ac6406109',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_59a4ac6406109')) {function content_59a4ac6406109($_smarty_tpl) {?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta name="Generator" content="YONGDA v1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="Keywords" content="YONGDA" />
+        <meta name="Description" content="YONGDA" />
+        
+        <title>Y ONGDA - Powered by YongDa</title>
+        
+        <link href="<?php echo @CSS_URL;?>
+style.css" rel="stylesheet" type="text/css" />
+        
+    </head>
+    
+    abcdefddd
+
+</html><?php }} ?>
