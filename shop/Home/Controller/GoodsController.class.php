6421c6989a906c5eeed89c06574bc53908ba463c
family <2224168716@qq.com>
2017-10-17 07:24:59 +0800

    add memcached

--- shop/Home/Controller/GoodsController.class.php
+++ shop/Home/Controller/GoodsController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class GoodsController extends Controller {
     function showlist(){
+//        连接到memcache服务器
+
         $this->display();
     }
 }
