095702115d2484a2b8a10f8e63ae20436b09ec5c
family <2224168716@qq.com>
2017-10-17 22:26:31 +0800

    memcached ok

--- shop/Home/Controller/GoodsController.class.php
+++ shop/Home/Controller/GoodsController.class.php
@@ -4,7 +4,19 @@ use Think\Controller;
 class GoodsController extends Controller {
     function showlist(){
 //        连接到memcache服务器
-
+        S(array('type'=>'memcache','host'=>'127.0.0.1','port'=>11211));
+//        首先到memcache里面获取缓存信息
+        $info = S('goods_info_mem');
+//        判断$info是否存在
+//
+        if(empty($info)){
+            echo '从数据库中请求数据';
+//            从数据库中获得数据
+            $info = D('Goods')->select();
+//            把获得的数据设置到memcache中去
+            S('goods_info_mem',$info);
+        }
+        $this->assign('info',$info);
         $this->display();
     }
 }
--- shop/memcachetest.php
+++ shop/memcachetest.php
@@ -6,6 +6,6 @@
  * Time: 7:56
  */
 $mem = new Memcache();
-$flag = $mem -> connect('localhost',11211);
+$flag = $mem -> connect('127.0.0.1',11211);
 $f1 = $mem -> set('weather','cloud',0);
 var_dump($f1);//true
